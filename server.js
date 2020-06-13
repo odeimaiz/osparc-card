17399335102ce4eb64d9f9f48cbbf53656f0ca62
Odei Maiz <33152403+odeimaiz@users.noreply.github.com>
2020-06-13 20:33:15 +0200

    read info from Json url

--- server.js
+++ server.js
@@ -4,21 +4,21 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const https = require('https');
 const svgTools = require('@iconify/tools');
 
-app.get('/card', (req, res) => {
-  "https://raw.githubusercontent.com/pcrespov/osparc-sample-studies/master/Sleepers%20A%20lot/project.json"
-  const title = req.query.title;
-  console.log("title", title);
-  const description = req.query.description;
-  const thumbnail = req.query.thumbnail;
+const sendCustomCard = (res, title, description, thumbnail) => {
   const card_path = "C:/Develop/oSPARC_card/public/osparc_card.svg";
-  const new_card_path = "C:/Develop/oSPARC_card/public/osparc_new_card.svg";
+  const new_card_path = "C:/Develop/oSPARC_card/public/osparc_card_custom.svg";
   svgTools.ImportSVG(card_path)
     .then(svg => {
       let newSVG = svg.toString();
-      newSVG = newSVG.replace("Study_Title", title);
-      newSVG = newSVG.replace("Study_Description", description);
+      const defTitle = "Study_Title";
+      const defDescription = "Study_Description";
+      const defThumbnail = "https://osparc.io/osparc/../resource/osparc/osparc-white.svg";
+      newSVG = newSVG.replace(defTitle, title || defTitle);
+      newSVG = newSVG.replace(defDescription, description || defDescription);
+      newSVG = newSVG.replace(defThumbnail, thumbnail || defThumbnail);
       svgTools.ExportSVG(newSVG, new_card_path)
         .then(() => {
           res.sendFile(new_card_path);
@@ -29,6 +29,29 @@ app.get('/card', (req, res) => {
     }).catch(err => {
       console.error(err);
     });
+};
+
+app.get('/card', (req, res) => {
+  // const url = "https://raw.githubusercontent.com/pcrespov/osparc-sample-studies/master/Sleepers%20A%20lot/project.json";
+  const url = req.query.studyUrl;
+  const reqStudy = https.get(url, (resProject) => {
+    let data = '';
+    resProject.on('data', (stream) => {
+      data += stream;
+    });
+    resProject.on('end', () => {
+      const json_data = JSON.parse(data);
+      const title = json_data["name"];
+      console.log("title", title);
+      const description = json_data["description"];
+      const thumbnail = json_data["thumbnail"];
+      sendCustomCard(res, title, description, thumbnail);
+    });
+  });
+
+  reqStudy.on('error', (e) => {
+    console.err('reqProject', e.message);
+  });
 });
 
 const server = app.listen(8080);
